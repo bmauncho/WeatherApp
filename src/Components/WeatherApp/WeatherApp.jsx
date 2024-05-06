b330185e191db36b9f3f1b2653b2742fdc9386ed
Bradley_Adelbert <bmauncho@gmail.com>
2024-05-06 19:31:20 +0300

    second commit

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -14,12 +14,35 @@ import wind_icon from "../Assets/wind.png";
 const WeatherApp = () => {
   return (
     <div className= "container">
-      <div className= "top-bar">
-        <input type= "text" className = "cityInput" placeholder="Search"/>
-        <div className= "search-icon">
-            <img src={search_icon} alt=""/>
+        <div className= "top-bar">
+            <input type= "text" className = "cityInput" placeholder="Search"/>
+            <div className= "search-icon">
+                <img src={search_icon} alt=""/>
+            </div>
         </div>
-      </div>g
+        <div className= "weather-image">
+            <img src={cloud_icon} alt= ""/>
+        </div>
+        <div className= "weather-temp">25°c</div>
+        <div className= "weather-location">London</div>
+        <div className= "data-container">
+            <div className= "element">
+                <img src={humidity_icon} alt ="" className="icon"/>
+                <div className="data">
+                    <div className="humidity-percent">64%</div>
+                    <div className="text">Humidity</div>
+                </div>
+                
+            </div>
+            <div className= "element">
+                <img src={wind_icon} alt ="" className="icon"/>
+                <div className="data">
+                    <div className="wind-rate">18 km/h</div>
+                    <div className="text">Wind Speed</div>
+                </div>
+                
+            </div>
+        </div>
     </div>
   )
 }
